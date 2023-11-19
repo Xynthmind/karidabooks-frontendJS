b15ca679935c78e645ca04add52306933ed675a7
Victor Glez <77651273+Xynthmind@users.noreply.github.com>
2023-11-19 01:11:39 -0600

    Create NavBarBotCat.js
    Special component for the new view

---
+++ src/components/Widgets/NavBarBotCat.js
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../models/UserContext";
+import { KaridaBooksAPI } from "../../components/constants/API";
+//Iconos
+import { BsFillHouseFill, BsFillBookmarksFill, BsFillCartFill, BsFillPersonFill, BsFilePerson, BsPersonVcard } from "react-icons/bs"
+import { Button } from "react-bootstrap";
+import { colors } from "../constants/ColorsOfCompany";
+
+
+export default function NavBarBotCat() {
+    const { user, login, logout } = useContext(UserContext);
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch(`${KaridaBooksAPI}categories`)
+            .then(response => response.json())
+            .then(data => {
+                setCategories(data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+    return (
+        <div style={{ width: "100%", height: "5vh", backgroundColor: "#4d4861", alignItems: "center", justifyContent: "space-between", display: "flex", padding: 20 }}>
+            <Link to={"/cart"} style={{ textDecoration: "none" }}>
+                <Button style={{ backgroundColor: colors.primary, borderColor: colors.primary, marginRight: 16, alignItems: "center", display: "flex" }} type="submit">
+                    <BsFillCartFill size={24} style={{ marginRight: 6 }} />
+                    Cart
+                </Button>
+            </Link>
+        </div >
+    )
+}
