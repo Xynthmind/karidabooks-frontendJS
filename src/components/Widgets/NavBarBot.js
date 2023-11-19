2bc5b8110a0dbf0a217bce415b2ac3eb906dc76b
Victor Glez <77651273+Xynthmind@users.noreply.github.com>
2023-11-18 20:04:29 -0600

    Update NavBarBot.js

--- src/components/Widgets/NavBarBot.js
+++ src/components/Widgets/NavBarBot.js
@@ -1,7 +1,7 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../models/UserContext";
-import { categories } from "../../data/Categories";
+import { KaridaBooksAPI } from "../../components/constants/API";
 //Iconos
 import { BsFillHouseFill, BsFillBookmarksFill, BsFillCartFill, BsFillPersonFill, BsFilePerson, BsPersonVcard } from "react-icons/bs"
 import { Button, Dropdown } from "react-bootstrap";
@@ -9,20 +9,31 @@ import { colors } from "../constants/ColorsOfCompany";
 
 
 export default function NavBarBot() {
-
     const { user, login, logout } = useContext(UserContext);
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch(`${KaridaBooksAPI}categories`)
+          .then(response => response.json())
+          .then(data => {
+            console.log("DATA", data)
+            setCategories(data);
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }, []);
 
     return (
         <div style={{ width: "100%", height: "5vh", backgroundColor: "#4d4861", alignItems: "center", justifyContent: "space-between", display: "flex", padding: 20 }}>
             <Dropdown>
                 <Dropdown.Toggle style={{ backgroundColor: colors.primary, borderColor: colors.primary, marginRight: 16, alignItems: "center", display: "flex" }}>
                     <BsFillBookmarksFill size={24} style={{ marginRight: 6 }} />
-                    Categorias
+                    Categories
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                    {categories.map((cat) => {
+                    {categories.map((category) => {
                         return (
-                            <Dropdown.Item key={cat} style={{ backgroundColor: colors.white }}>{cat}</Dropdown.Item>
+                            <Dropdown.Item key={category.category_name} style={{ backgroundColor: colors.white }}>{category.category_name}</Dropdown.Item>
                         )
                     })}
                 </Dropdown.Menu>
@@ -30,7 +41,7 @@ export default function NavBarBot() {
             <Link to={"/cart"} style={{ textDecoration: "none" }}>
                 <Button style={{ backgroundColor: colors.primary, borderColor: colors.primary, marginRight: 16, alignItems: "center", display: "flex" }} type="submit">
                     <BsFillCartFill size={24} style={{ marginRight: 6 }} />
-                    Carrito
+                    Cart
                 </Button>
             </Link>
         </div >
