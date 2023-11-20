19572428071f180f96d0eaa162dc50a4784a8211
Victor Glez <77651273+Xynthmind@users.noreply.github.com>
2023-11-20 03:28:31 -0600

    Update Card.js
    New fonts

--- src/components/Widgets/Card.js
+++ src/components/Widgets/Card.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Image } from "react-bootstrap";
 import { colors, fontFamily } from "../constants/ColorsOfCompany";
+import "../constants/fonts.css"
 
 export default function CardBooks(props) {
     const { title, author, price, editorial, stock, photo } = props.data;
@@ -9,13 +10,13 @@ export default function CardBooks(props) {
     return (
         <Link to={`/book/${title}`} state={{ book: book }} style={{ width: "50%" , borderRadius: 10, alignItems: "center", justifyContent: "center", 
                     display: "flex", flexDirection: "column", boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.25)', color: "inherit", textDecoration: "none", marginLeft: "35%" }}>
-            <div style={{ backgroundColor: colors.white, borderRadius: 10, alignItems: "center", justifyContent: "center", display: "flex", flexDirection: "column"}}>
+            <div className="labels" style={{ backgroundColor: colors.white, borderRadius: 10, alignItems: "center", justifyContent: "center", display: "flex", flexDirection: "column"}}>
                 <Image src={photo } width={"100%"}  style={{ borderRadius: 10 }}></Image>
-                <div style={{ fontSize: 16, fontFamily: fontFamily.primary }}>{title}</div>
-                <div style={{ fontSize: 12, fontFamily: fontFamily.primary }}>{author}</div>
-                <div style={{ fontSize: 14, fontFamily: fontFamily.primary }}>{editorial}</div>
-                <div style={{ fontSize: 16, fontFamily: fontFamily.primary }}>Pieces avalible: {stock}</div>
-                <div style={{ fontSize: 16, fontFamily: fontFamily.primary }}>Price: ${price}</div>
+                <div className="labels" style={{ fontSize: 16 }}>{title}</div>
+                <div className="labels" style={{ fontSize: 12 }}>{author}</div>
+                <div className="labels" style={{ fontSize: 14 }}>{editorial}</div>
+                <div className="labels" style={{ fontSize: 16 }}>Pieces avalible: {stock}</div>
+                <div className="labels" style={{ fontSize: 16 }}>Price: ${price}</div>
             </div>
         </Link>
     )
