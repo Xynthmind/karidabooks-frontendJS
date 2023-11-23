ce019f634037f9255fa48e6e95c36902591d3f27
Victor Glez <77651273+Xynthmind@users.noreply.github.com>
2023-11-23 11:26:20 -0600

    Update App.js
    New views added

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import { UserProvider } from './components/models/UserContext';
 import Home from './views/HomePages/Home';
 import Login from './views/HomePages/Login';
 import Register from './views/HomePages/Register';
+import Book from './views/Books/Book';
 import BookPerCategory from './views/Books/BookPerCategory';
 import BookSearching from './views/Books/BooksSearching';
+import Cart from './views/Books/Cart';
 function App() {
   return (
     <UserProvider>
@@ -16,8 +18,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/books/:book" element={<Book />} />
           <Route path="/books/categoryclass" element={<BookPerCategory />} />
           <Route path="/books/searchingmode" element={<BookSearching />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
     </UserProvider>
