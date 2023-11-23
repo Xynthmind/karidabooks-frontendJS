1e21a0758b3528b22c601b66253eec5f6b93be17
Victor Glez <77651273+Xynthmind@users.noreply.github.com>
2023-11-23 11:26:33 -0600

    Update Card.js
    Routing problem solved

--- src/components/Widgets/Card.js
+++ src/components/Widgets/Card.js
@@ -8,7 +8,7 @@ export default function CardBooks(props) {
     const { title, author, price, editorial, stock, photo } = props.data;
     const book = props.data;
     return (
-        <Link to={`/book/${title}`} state={{ book: book }} style={{ width: "50%" , borderRadius: 10, alignItems: "center", justifyContent: "center", 
+        <Link to={`/books/${title}`} state={{ book: book }} style={{ width: "50%" , borderRadius: 10, alignItems: "center", justifyContent: "center", 
                     display: "flex", flexDirection: "column", boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.25)', color: "inherit", textDecoration: "none", marginLeft: "35%" }}>
             <div className="labels" style={{ backgroundColor: colors.white, borderRadius: 10, alignItems: "center", justifyContent: "center", display: "flex", flexDirection: "column"}}>
                 <Image src={photo } width={"100%"}  style={{ borderRadius: 10 }}></Image>
